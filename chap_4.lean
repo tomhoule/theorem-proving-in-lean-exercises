521b012c5c4bb339e7768b7c0a9c277eee4b358e
Tom Houlé <tom@tomhoule.com>
2020-05-02 08:21:05 +0200

    Fill in initial code for chap 4 exercises 6 and 7

--- chap_4.lean
+++ chap_4.lean
@@ -329,3 +329,37 @@ namespace ex5
     example : (∃ x, r → p x) ↔ (r → ∃ x, p x) := to_unite_all_peoples α p a r
 
 end ex5
+
+namespace ex6
+
+    variables (real : Type) [ordered_ring real]
+    variables (log exp : real → real)
+    variable  log_exp_eq : ∀ x, log (exp x) = x
+    variable  exp_log_eq : ∀ {x}, x > 0 → exp (log x) = x
+    variable  exp_pos    : ∀ x, exp x > 0
+    variable  exp_add    : ∀ x y, exp (x + y) = exp x * exp y
+
+    -- this ensures the assumptions are available in tactic proofs
+    include log_exp_eq exp_log_eq exp_pos exp_add
+
+    example (x y z : real) :
+    exp (x + y + z) = exp x * exp y * exp z :=
+    by rw [exp_add, exp_add]
+
+    example (y : real) (h : y > 0)  : exp (log y) = y :=
+    exp_log_eq h
+
+    theorem log_mul {x y : real} (hx : x > 0) (hy : y > 0) :
+    log (x * y) = log x + log y :=
+    sorry
+
+end ex6
+
+namespace ex7
+
+    #check sub_self
+
+    example (x : ℤ) : x * 0 = 0 :=
+    sorry
+
+end ex7
